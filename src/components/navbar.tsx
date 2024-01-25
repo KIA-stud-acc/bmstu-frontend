541d4b310d0bb21b401d4c3621878e6272c7cd0a
Илья <iliakekin52@gmail.com>
2024-01-25 08:09:52 +0300

    lkjhgfds

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -56,28 +56,28 @@ const SubmitLogout = async () =>{
 
 
   return (
-    <Navbar className="navbar-light navbar-expand" expand="lg" >
-      <Container fluid className="new flex-column" style={{width:"100%"}}>
+    <Navbar className="navbar-light" expand="xl" >
+      <Container fluid className="new" style={{width:"100%"}}>
         <Navbar.Brand as={Link} to="/bmstu-frontend/" className="brand text-center brandcss" style={{fontSize:"2em"}}>Выборы</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="w-100 d-flex justify-content-center ">
+        <Navbar.Collapse id="basic-navbar-nav" className="w-100 justify-content-center ">
           <Nav className="me-auto my-2 my-lg-0">
-            <Nav.Link as={Link} to="/bmstu-frontend/" className="li mx-3 left" style={{flex:"1"}}><p className='navtext'>Главная</p></Nav.Link>
-            <Nav.Link  as={Link} to="/bmstu-frontend/vybory" className="li mx-3 left" style={{flex:"1"}}>Каталог</Nav.Link>
-            <Nav.Link as={Link} to="#link" className="li mx-3 left" style={{flex:"1"}}>Контакты</Nav.Link>
-            {useIsLogged() && <><Nav.Link as={Link} to="/bmstu-frontend/applications" className="li mx-3 left" style={{flex:"1"}}>Голосования</Nav.Link></>}
-            {useIsModer() && <><Nav.Link as={Link} to="/bmstu-frontend/moder/vybory" className="li mx-3 left">Редактирование названий</Nav.Link></>}
+            <Nav.Link as={Link} to="/bmstu-frontend/" className="li mx-2 left"><p className='navtext'>Главная</p></Nav.Link>
+            <Nav.Link  as={Link} to="/bmstu-frontend/vybory" className="li mx-2 left">Каталог</Nav.Link>
+            <Nav.Link as={Link} to="#link" className="li mx-2 left">Контакты</Nav.Link>
+            {useIsLogged() && <><Nav.Link as={Link} to="/bmstu-frontend/applications" className="li mx-2 left">Голосования</Nav.Link></>}
+            {useIsModer() && <><Nav.Link as={Link} to="/bmstu-frontend/moder/vybory" className="li mx-2 left">Редактирование названий</Nav.Link></>}
           </Nav>
           <Nav className="d-flex me-2">
             <>
-            {useBasket()?<Nav.Link as={Link} to="/bmstu-frontend/applications/current" className="ml-auto rightnav">Черновик голосования</Nav.Link>:
-                <Nav.Link disabled className="ml-auto">Черновик голосования</Nav.Link>}
+            {useBasket()?<Nav.Link as={Link} to="/bmstu-frontend/applications/current" className="ml-auto mx-2 rightnav">Черновик голосования</Nav.Link>:
+                <Nav.Link disabled className="ml-auto mx-2 rightnav">Черновик голосования</Nav.Link>}
             {
               useIsLogged()?(
                 (<>
-                <Nav.Link onClick={()=>SubmitLogout()} className="ml-auto  rightnav">({username}) Выйти</Nav.Link></>)
-                ): <><Nav.Link as={Link} to="/bmstu-frontend/auth/reg" className="ml-auto rightnav">Регистрация</Nav.Link>
-                <Nav.Link as={Link} to="/bmstu-frontend/auth" className="ml-auto rightnav">Войти</Nav.Link></>
+                <Nav.Link onClick={()=>SubmitLogout()} className="ml-auto li mx-2 rightnav">({username}) Выйти</Nav.Link></>)
+                ): <><Nav.Link as={Link} to="/bmstu-frontend/auth/reg" className="ml-auto li mx-2 rightnav">Регистрация</Nav.Link>
+                <Nav.Link as={Link} to="/bmstu-frontend/auth" className="ml-auto li mx-2  rightnav">Войти</Nav.Link></>
             }</></Nav>
         </Navbar.Collapse>
       </Container>
